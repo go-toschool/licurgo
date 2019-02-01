aea0e3ad7e7a841244bd08a37a55d03cda2a0c98
mtavano <chelotavano.udp@gmail.com>
2019-02-01 17:00:35 -0300

    Delete leftover go struct

--- migrations/20190126102616_talks.sql
+++ migrations/20190126102616_talks.sql
@@ -1,18 +1,5 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-type Talk struct {
-	ID           string    `json:"id" db:"id"`
-	Title        string    `json:"title" db:"title"`
-	Description  string    `json:"description" db:"description"`
-	Repository   string    `json:"repository" db:"repository"`
-	Date         time.Time `json:"date" db:"date"`
-	Tags         string    `json:"tags" db:"tags"`
-	User_user_id string    `json:"user_id" db:"user_id"`
-
-	CreatedAt time.Time  `json:"created_at" db:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at" db:"updated_at"`
-	DeletedAt *time.Time `json:"-" db:"deleted_at"`
-}
 
 create table if not exists talks (
   id                       uuid primary key default gen_random_uuid(),
